b43bc16364aeb3dade439013965a3fe89c6d008c
ANGAD MAURYA <140787200+angadmaurya8354@users.noreply.github.com>
2024-05-18 12:56:52 +0530

    Add files via upload

---
+++ services.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>services</title>
+    <link rel="stylesheet" href="services.css">
+    <!-- icon link*************** -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="image/logo.png" alt="" />
+            </div>
+            <div class="menu" id="menu">
+                <a href="index.php">HOME</a>
+                <a href="about.php">ABOUT</a>
+                <a href="services.php">SERVICES</a>
+                <a href="contact.php">CONTACT</a>
+            </div>
+            <div> <a href="#" class="whats">202-555-7890</a></div>
+            <div class="menu-icon">
+                <i class="bi bi-card-text" onclick="toggleMenu()"></i>
+            </div>
+
+        </nav>
+        <div class="header_data">
+            <h1>Our Sevices
+
+            </h1>
+            <h2><br>What We do</h2>
+
+        </div>
+    </header>
+    <!-- section-1****************************** -->
+    <section>
+        <div class="service">
+            <div> <img src="image/2-home.jpg" height="420px" width="320px"> </div>
+            <div>
+                <p class="form">From $19</p>
+                <h3>Web Design</h3>
+                <p> Tell them exactly what this service will do for them. <br> <br>
+
+                    Focus on the direct benefits to the user, rather than just <br> listing the features of your
+                    service. You can also address <br> any objections you think they might have. <br> <br>
+
+                    We make beautiful websites that get you leads and sales. <br>
+                    Keep your target audience in mind, and keep it simple
+                    <br>
+                </p>
+                <a class="service_btn" href="#">Get A Quote</a>
+            </div>
+        </div>
+        <div class="service">
+            <div> <img src="image/2-home.jpg" height="420px" width="320px"> </div>
+            <div>
+                <p class="form">From $19</p>
+                <h3>Web Design</h3>
+                <p> Tell them exactly what this service will do for them. <br> <br>
+
+                    Focus on the direct benefits to the user, rather than just <br> listing the features of your
+                    service. You can also address <br> any objections you think they might have. <br> <br>
+
+                    We make beautiful websites that get you leads and sales. <br>
+                    Keep your target audience in mind, and keep it simple
+                    <br>
+                </p>
+                <a class="service_btn" href="#">Get A Quote</a>
+            </div>
+        </div>
+        <div class="service">
+            <div> <img src="image/2-home.jpg" height="420px" width="320px"> </div>
+            <div>
+                <p class="form">From $19</p>
+                <h3>Web Design</h3>
+                <p> Tell them exactly what this service will do for them. <br> <br>
+
+                    Focus on the direct benefits to the user, rather than just <br> listing the features of your
+                    service. You can also address <br> any objections you think they might have. <br> <br>
+
+                    We make beautiful websites that get you leads and sales. <br>
+                    Keep your target audience in mind, and keep it simple
+                    <br>
+                </p>
+                <a class="service_btn" href="#">Get A Quote</a>
+            </div>
+        </div>
+    </section>
+    <!-- section-2************************* -->
+    <section class="call">
+        <h1>Work with me</h1>
+        <p>Send me a message to discuss your needs. <br> I always reply within 24 hours.</p>
+        <a href="#">Get A Quote</a>
+
+    </section>
+    <footer class="foot">
+        <div class="menu1">
+            <a href="index.php">HOME</a>
+            <a href="about.php">ABOUT</a>
+            <a href="services.php">SERVICES</a>
+            <a href="contact.php">CONTACT</a>
+        </div>
+        <div>
+            <img src="image/logo-black-1.png" height="auto" width="60px">
+        </div>
+        <div>
+            &copy;2024 Hope Starter Design
+        </div>
+    </footer>
+
+
+    <script src="service.js"></script>
+</body>
+
+</html>
