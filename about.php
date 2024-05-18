5b4c689f129d2d3a6ece4ab62ab9ccf2820799f3
ANGAD MAURYA <140787200+angadmaurya8354@users.noreply.github.com>
2024-05-18 12:50:07 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About</title>
+    <link rel="stylesheet" href="about.css">
+    <link rel="stylesheet" href="style.css">
+    <!-- icon link*************** -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="logo">
+                <img src="image/logo.png" alt="" />
+            </div>
+            <div class="menu" id="menu">
+                <a href=" index.php">HOME</a>
+                <a href="about.php">ABOUT</a>
+                <a href="services.php">SERVICES</a>
+                <a href="contact.php">CONTACT</a>
+            </div>
+            <div> <a href="#" class="whats">202-555-7890</a></div>
+            <div class="menu-icon">
+                <i class="bi bi-card-text" onclick="toggleMenu()"></i>
+            </div>
+        </nav>
+        <div class="header_data">
+            <h1>About Us</h1>
+
+
+        </div>
+    </header>
+
+    <!-- section-1************************** -->
+    <section class="welcome">
+        <div class="wlc1"><img src="image/6-home.jpg" width="370px" height="500px">
+            <h3>Dragos Andrei Atudorei</h3>
+            <p>WordPress Content Creator <br>
+                Certified Web Designer <br>
+                Incurable Perfectionist</p>
+        </div>
+        <div class="wlc_data">
+            <p class="wlc">WELCOME!</p>
+            <h1>My name is Andrei and I am <br> the creator of this template.</h1>
+            <p>I gave it my all to make this the best design to start from. <br> <b> I named it Hope</b>, in honor of my
+                <br> client who inspired me to make it – her middle name is <b>Hope</b>. <br><br>
+                For the first section at the top of the page, I recommend <br> writing something that will <b> pique the
+                    interest </b>of your <br> target audience. Make them curious to read more! <br><br>
+
+                These middle sections are the perfect place to introduce <br> yourself, and to perhaps start <b>
+                    introducing your service</b> <br><br>
+
+                The last section below is great for a call to action.
+            </p>
+            <a class="wlc_btn1" href="#">See My Services</a>
+            <a class="wlc_btn2" href="#">More About Me</a>
+        </div>
+
+
+
+    </section>
+
+    <h1 class="wlc_h"> gave it my all to try and make this the best possible design that you can start from.</h1>
+
+    <section class="welcome">
+
+        <div class="wlc_data">
+
+            <h1>I love nature, and so Hope is a <br> design inspired by nature.</h1>
+            <p>But we’ve used it to build sites for psychologists, doctors, <br> restaurants, lawyers, personal
+                trainers, gyms, and more. <br><br>
+
+                <b> It is also one of the first templates </b> built using the new <br> Container. This means your
+                website will load super fast. <br><br>
+
+                It took many people hundreds of hours of work, and lots <br> of love, to make this template and tutorial
+                possible. <br><br>
+
+                <b> But your comments on our videos </b>, like the one you can <br> see below, make it all worth it.
+                Thank you so much!
+            </p>
+
+        </div>
+        <div class="wlc1"><img src="image/2-about.jpg" width="370px" height="500px">
+
+        </div>
+
+    </section>
+    <h1 class="wlc_h">I sincerely hope this helps you make a <br> website that you can be truly proud of!</h1>
+
+    <!-- section-2****************************** -->
+    <section class="client_reviwe">
+        <img src="image/3-about.jpg" alt="">
+        <p>Ruth A. Flora</p>
+        <p style="color:  #0E5735;">◆◆◆◆◆</p>
+        <h2>You can showcase a client review here. Make it nice <br> and short, then use this button to <br> send them
+            to your reviews or services page.</h2>
+        <a href="#">What My Clients Say</a>
+
+    </section>
+    <footer class="foot">
+        <div class="menu1">
+            <a href="index.php">HOME</a>
+            <a href="about.php">ABOUT</a>
+            <a href="services.php">SERVICES</a>
+            <a href="contact.php">CONTACT</a>
+        </div>
+        <div>
+            <img src="image/logo-black-1.png" height="auto" width="60px">
+        </div>
+        <div>
+            &copy;2024 Hope Starter Design
+        </div>
+    </footer>
+    <script src="index.js"></script>
+
+</body>
+
+</html>
