5d66995e9a62d649b674cadc481b50bbfa855fb9
ANGAD MAURYA <140787200+angadmaurya8354@users.noreply.github.com>
2024-05-18 12:53:52 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+// let bi = document.getElementsByClassName(bi){
+
+// }
+let menulist = document.getElementById("menu");
+menulist.style.maxHeight = "0px";
+
+function toggleMenu() {
+    if (menulist.style.maxHeight == "0px") {
+        menulist.style.maxHeight = "400px";
+    }
+    else {
+        menulist.style.maxHeight = "0px";
+    }
+}
